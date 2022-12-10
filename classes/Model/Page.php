cefbc8324ab95038fa1f9fd44a87828aa17310a1
PHetem <p_hetem@hotmail.com>
2022-12-10 18:07:46 -0300

    Add page model

---
+++ classes/Model/Page.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace API\Model;
+
+class Page {
+
+    public $Url;
+
+    public $Selector;
+    public $SelectorRemove;
+    public $SelectorRecursive;
+
+    public $Title;
+    public $Content;
+
+    public $RecursiveUrls;
+}
